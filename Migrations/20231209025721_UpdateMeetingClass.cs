1aa1e791a55ae07f37d6d4e0a3a2cd3310f97442
jamesjavanyc` <1931736857@qq.com>
2023-12-08 21:59:08 -0500

    Refactor meeting entity

---
+++ Migrations/20231209025721_UpdateMeetingClass.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SacramentMeetingPlanner.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMeetingClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClosingPrayer",
+                table: "Meetings",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClosingSong",
+                table: "Meetings",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Hymn",
+                table: "Meetings",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Leader",
+                table: "Meetings",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MusicNumber",
+                table: "Meetings",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OpeningPrayer",
+                table: "Meetings",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OpeningSong",
+                table: "Meetings",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClosingPrayer",
+                table: "Meetings");
+
+            migrationBuilder.DropColumn(
+                name: "ClosingSong",
+                table: "Meetings");
+
+            migrationBuilder.DropColumn(
+                name: "Hymn",
+                table: "Meetings");
+
+            migrationBuilder.DropColumn(
+                name: "Leader",
+                table: "Meetings");
+
+            migrationBuilder.DropColumn(
+                name: "MusicNumber",
+                table: "Meetings");
+
+            migrationBuilder.DropColumn(
+                name: "OpeningPrayer",
+                table: "Meetings");
+
+            migrationBuilder.DropColumn(
+                name: "OpeningSong",
+                table: "Meetings");
+        }
+    }
+}
--- Models/Meeting.cs
+++ Models/Meeting.cs
@@ -6,12 +6,38 @@ namespace SacramentMeetingPlanner.Models
     {
         public int Id { get; set; }
 
-        // Things specific to the meeting.
-
         [Required]
         [StringLength(250)]
         [Display(Name = "Ward")]
         public string Ward { get; set; } = "";
+        [Required]
+        [StringLength(250)]
+        [Display(Name = "Leader")]
+        public string Leader { get; set; } = "";
+        [Required]
+        [StringLength(250)]
+        [Display(Name = "Opening Song")]
+        public string OpeningSong { get; set; } = "";
+        [Required]
+        [StringLength(250)]
+        [Display(Name = "Closing Song")]
+        public string ClosingSong { get; set; } = "";
+        [Required]
+        [StringLength(250)]
+        [Display(Name = "Opening Prayer")]
+        public string OpeningPrayer { get; set; } = "";
+        [Required]
+        [StringLength(250)]
+        [Display(Name = "Closing Prayer")]
+        public string ClosingPrayer { get; set; } = "";
+        [Required]
+        [StringLength(250)]
+        [Display(Name = "Hymn")]
+        public string Hymn { get; set; } = "";
+        [Required]
+        [StringLength(250)]
+        [Display(Name = "Music Number")]
+        public string MusicNumber { get; set; } = "";
 
         [Required]
         [DataType(DataType.Date)]
@@ -23,7 +49,5 @@ namespace SacramentMeetingPlanner.Models
         [Display(Name = "Address")]
         public string? Address { get; set; }
 
-        // OPTIONAL MEETING TYPE
-        // public int Type { get; set; }
     }
 }
