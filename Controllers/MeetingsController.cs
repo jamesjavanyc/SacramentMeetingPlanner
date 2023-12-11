340e3ab1f9a27feecb0c472a4efc2ea8ba6a2165
exagent957 <exagent957@gmail.com>
2023-12-11 11:17:57 -0600

    removed ward and address from controller

--- Controllers/MeetingsController.cs
+++ Controllers/MeetingsController.cs
@@ -56,7 +56,7 @@ namespace SacramentMeetingPlanner.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Ward,Date,Address, Leader, OpeningSong, ClosingSong, OpeningPrayer, ClosingPrayer, SacramentHymn, MusicNumber")] Meeting meeting)
+        public async Task<IActionResult> Create([Bind("Id, Date, Leader, OpeningSong, ClosingSong, OpeningPrayer, ClosingPrayer, SacramentHymn, MusicNumber")] Meeting meeting)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace SacramentMeetingPlanner.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Ward,Date,Address, Leader, OpeningSong, ClosingSong, OpeningPrayer, ClosingPrayer, SacramentHymn, MusicNumber")] Meeting meeting)
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Date, Leader, OpeningSong, ClosingSong, OpeningPrayer, ClosingPrayer, SacramentHymn, MusicNumber")] Meeting meeting)
         {
             if (id != meeting.Id)
             {
